6746a18978b07555cd4eac9912dd5dcadcdcbd3d
Vineeth Vattikuti <vinkriv@gmail.com>
2016-11-21 11:52:57 -0600

    Proj

--- app/src/main/java/com/example/a10017404/listviewproj/MainActivity.java
+++ app/src/main/java/com/example/a10017404/listviewproj/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.a10017404.listviewproj;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listView1;
+    ListView listView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView1=(ListView)(findViewById(R.id.listView1));
+        listView2=(ListView)(findViewById(R.id.listView2));
 
     }
 }
